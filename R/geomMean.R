855badb29b30f88695660ce2bafab9b68ba16d2d
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2022-07-29 14:30:49 -0300

    first commit

---
+++ R/geomMean.R
@@ -0,0 +1,20 @@
+#' Calcular a média geométrica
+#'
+#' Esta função calculará a média geométrica de um vetor numérico
+#'
+#' @param x Um vetor numérico
+#' @param dec O número de casas decimais (padrão: 3)
+#'
+#' @examples
+#'
+#' geomMean(1:6, 2) # Vetor
+#'
+#' geomMean(mtcars$mpg) # DataFrame
+#'
+#' mtcars %>% summarise(media = geomMean(mpg)) # Dplyr
+#'
+#' @export
+
+geomMean <- function(x, dec = 3){
+  round(prod(x, na.rm = T)^(1/length(x[!is.na(x)])), dec)
+}
