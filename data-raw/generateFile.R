9af036bf849eae62b2c019820cbafbdcf34ba96f
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2022-08-19 14:23:54 -0300

    Ajuste dt_meses

--- data-raw/generateFile.R
+++ data-raw/generateFile.R
@@ -1,7 +1,7 @@
 # Load raw data from .csv file
 dt_contabil <- read.csv("data-raw/ratio.csv", sep = ";")
 names(dt_contabil)[1] <- "empresa"
-dt_meses <- read.csv("data-raw/meses.csv", sep = ";")
+dt_meses <- read.csv("data-raw/meses.csv")
 # Apply preprocessing...
 # Save the cleaned data in the required R package location
 usethis::use_data(dt_contabil, overwrite = T)
