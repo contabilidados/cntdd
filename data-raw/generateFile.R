4f2c0ee91f99195755b2df389991d57f48bd8cf9
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2022-09-20 17:10:25 -0300

    ajuste dt_contabil

--- data-raw/generateFile.R
+++ data-raw/generateFile.R
@@ -1,5 +1,5 @@
 # Load raw data from .csv file
-dt_contabil <- read.csv("data-raw/ratio.csv", sep = ";")
+dt_contabil <- read.csv("data-raw/ratio.csv", sep = ";", na.strings = "-")
 names(dt_contabil)[1] <- "empresa"
 dt_meses <- read.csv("data-raw/meses.csv", encoding = "UTF-8")
 # Apply preprocessing...
