cb8329019c7b250381a9125552b443efb81d2bd0
kleberformiga <mirandakf@ufersa.edu.br>
2023-03-14 23:04:03 -0300

    inclui o banco de dados ufRegiao

--- data-raw/generateFile.R
+++ data-raw/generateFile.R
@@ -2,7 +2,9 @@
 dt_contabil <- read.csv("data-raw/ratio.csv", sep = ";", na.strings = "-")
 names(dt_contabil)[1] <- "empresa"
 dt_meses <- read.csv("data-raw/meses.csv", encoding = "UTF-8")
+dt_ufRegiao <- read.csv("data-raw/ufRegiao.csv", encoding = "latim-1", sep = ";")
 # Apply preprocessing...
 # Save the cleaned data in the required R package location
 usethis::use_data(dt_contabil, overwrite = T)
 usethis::use_data(dt_meses, overwrite = T)
+usethis::use_data(dt_ufRegiao, overwrite = T)
