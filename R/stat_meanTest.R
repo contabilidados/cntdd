b53d80c735ba65af54629f42e96c455f099dee7d
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2023-06-16 11:28:34 -0300

    mudanca no teste de media para considerar a diferenca de mediana quando nao ha distribuicao normal em pelo menos uma das variaveis

--- R/stat_meanTest.R
+++ R/stat_meanTest.R
@@ -29,7 +29,7 @@
 #' @import dplyr
 #' @export
 
-stat_meanTest <- function(value_Y, name_Y, value_X, name_X, pvalor = 0.05){
+stat_meanTest <- function(value_Y, name_Y = "Y", value_X, name_X = "X", pvalor = 0.05){
 
   A <- value_Y
   B <- value_X
@@ -48,15 +48,24 @@ stat_meanTest <- function(value_Y, name_Y, value_X, name_X, pvalor = 0.05){
 
   if(pvVarAB > pvalor) {parVar <- T} else {parVar <- F}
 
-  if(pvNormA > pvalor & pvNormB > pvalor) {difmedia <- "Teste t";
-  pvteste <- t.test(A, B, var.equal = parVar);
-  Normalidade <- "Ambos grupos tem distribuição normal"} else {difmedia <- "Mann-Whitney";
-  pvteste <- suppressMessages(suppressWarnings(wilcox.test(A, B)));
-  Normalidade <- "Pelo menos um dos grupos não tem distribuição normal"}
+  if(pvNormA > pvalor & pvNormB > pvalor) {
+    difmedia <- "Teste t";
+    pvteste <- t.test(A, B, var.equal = parVar);
+    Normalidade <- "Ambos grupos tem distribuição normal";
+    RESULT <- "Médias"
+    } else {
+      difmedia <- "Mann-Whitney";
+      pvteste <- suppressMessages(suppressWarnings(wilcox.test(A, B)));
+      Normalidade <- "Pelo menos um dos grupos não tem distribuição normal";
+      RESULT <- "Medianas"
+      }
 
-  if(pvteste$p.value > pvalor) {result <- "Médias iguais"} else {result <- "Médias diferentes"}
+  if(pvteste$p.value > pvalor) {result <- paste0(RESULT, " iguais")
+  } else {
+    result <- paste0(RESULT, " diferentes")}
 
-  spread <- mean(A, na.rm = T) - mean(B, na.rm = T)
+  spreadMedia <- mean(A, na.rm = T) - mean(B, na.rm = T)
+  spreadMediana <- median(A, na.rm = T) - median(B, na.rm = T)
 
   dados <- data.frame(
     normA = formatC(pvNormA, digits = 3, format = "f", decimal.mark = ","),
@@ -67,7 +76,10 @@ stat_meanTest <- function(value_Y, name_Y, value_X, name_X, pvalor = 0.05){
     resultado = result,
     mediaA = formatC(mean(A, na.rm = T), digits = 3, format = "f", decimal.mark = ","),
     mediaB = formatC(mean(B, na.rm = T), digits = 3, format = "f", decimal.mark = ","),
-    spread = formatC(spread, digits = 3, format = "f", decimal.mark = ","),
+    spreadMedia = formatC(spreadMedia, digits = 3, format = "f", decimal.mark = ","),
+    medianaA = formatC(median(A, na.rm = T), digits = 3, format = "f", decimal.mark = ","),
+    medianaB = formatC(median(B, na.rm = T), digits = 3, format = "f", decimal.mark = ","),
+    spreadMediana = formatC(spreadMediana, digits = 3, format = "f", decimal.mark = ","),
     qdeObs = paste0(name_a, ": ", qdeA, " | ", name_b, ": ", qdeB))
 
   tabDados <- as.data.frame(t(dados))
@@ -80,7 +92,9 @@ stat_meanTest <- function(value_Y, name_Y, value_X, name_X, pvalor = 0.05){
     paste0("pValue ", difmedia),
     paste0("Resultado ", difmedia),
     paste0("Média de ", name_a), paste0("Média de ", name_b),
-    paste0(name_a, " menos ", name_b), "Qde de Obs"
+    paste0("Média: ", name_a, " menos ", name_b),
+    paste0("Mediana de ", name_a), paste0("Mediana de ", name_b),
+    paste0("Mediana: ", name_a, " menos ", name_b), "Qde de Obs"
   )
 
   colnames(tabDados) <- "Resultados"
