b2e4922948c4891ab81159387a397ebdf71648b4
kleberformiga <mirandakf@ufersa.edu.br>
2024-04-12 14:32:42 -0300

    inclusao de novas bases

--- data-raw/generateFile.R
+++ data-raw/generateFile.R
@@ -9,9 +9,12 @@ dt_cnpjCodB3 <-
     cnpj = cntdd::utl_CNPJ_Mascara(cnpj),
     codB3 = substr(codB3, 1, 4))
 dt_ufRegiao <- read.csv("data-raw/ufRegiao.csv", encoding = "latim-1", sep = ";")
+dt_cadastroCVM <- read.csv("data-raw/dtCadastroCVM.csv", encoding = "latim-1", sep = ",")
 # Apply preprocessing...
 # Save the cleaned data in the required R package location
 usethis::use_data(dt_contabil, overwrite = T)
 usethis::use_data(dt_meses, overwrite = T)
 usethis::use_data(dt_cnpjCodB3, overwrite = T)
 usethis::use_data(dt_ufRegiao, overwrite = T)
+usethis::use_data(dt_cadastroCVM, overwrite = T)
+load("data/dt_cadastroCVM.rda")
