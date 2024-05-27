1120944525ff221d2025a2db308309a06f1df541
alexsandroprado <alexsandro.prado@ufersa.edu.br>
2024-05-27 17:57:26 -0300

    atualizacao funcao consultar_CNPJ e adicao da base iseB3

---
+++ R/dt_iseB3.R
@@ -0,0 +1,20 @@
+#' @title Conjunto de Dados ISE B3 (2021-2023)
+#'
+#' @description
+#' Dados de desempenho ESG das empresas participantes dos processos de seleção
+#' às carteiras 2021 e 2023 do ISE B3 disponibilizadas no módulo "ESG Índices
+#' da Plataforma ESG Workspace.
+#'
+#' @details
+#' Informações adicionais sobre como usar o pacote, orientamos acessar o menu
+#' `cntdd` do Blog do Projeto contabiliDados: <https://contabilidados.quarto.pub>.
+#' Ao acessar, fazer busca pelo nome da funcao `dt_contabil`
+#'
+#' Contatos pelo email do Projeto contabiliDados:
+#' Email: <contabilidados@@ufersa.edu.br>
+#' Siga-nos no Instagram: <https://www.instagram.com/contabilidados> @contabilidados
+#'
+#' @format
+#' Data.frame com 40 linhas e 35 colunas.
+#'
+"dt_iseB3"
--- data-raw/generateFile.R
+++ data-raw/generateFile.R
@@ -4,6 +4,7 @@ names(dt_contabil)[1] <- "empresa"
 dt_meses <- read.csv("data-raw/meses.csv", encoding = "UTF-8")
 dt_ufRegiao <- read.csv("data-raw/ufRegiao.csv", encoding = "latin1", sep = ";")
 dt_cvmB3 <- read.csv("data-raw/dtCVMB3.csv", encoding = "latim-1", sep = ",")
+dt_iseB3 <- read.csv("data-raw/iseb3score.csv", encoding = "latin1", sep = ";")
 dt_cvmB3$descricaoAtividade <- iconv(dt_cvmB3$descricaoAtividade, from = 'UTF-8', to = 'ASCII//TRANSLIT')
 dt_cvmB3$setorCVM <- iconv(dt_cvmB3$setorCVM, from = 'UTF-8', to = 'ASCII//TRANSLIT')
 dt_cvmB3$nomeEmpresarial <- iconv(dt_cvmB3$nomeEmpresarial, from = 'UTF-8', to = 'ASCII//TRANSLIT')
@@ -11,10 +12,12 @@ dt_cvmB3$descricaoAtividade <- stringr::str_to_sentence(dt_cvmB3$descricaoAtivid
 dt_cvmB3$setorCVM <- stringr::str_to_sentence(dt_cvmB3$setorCVM)
 dt_cvmB3$nomeEmpresarial <- toupper(dt_cvmB3$nomeEmpresarial)
 
+
 # Apply preprocessing...
 # Save the cleaned data in the required R package location
 usethis::use_data(dt_contabil, overwrite = T)
 usethis::use_data(dt_meses, overwrite = T)
 usethis::use_data(dt_ufRegiao, overwrite = T)
 usethis::use_data(dt_cvmB3, overwrite = T)
+usethis::use_data(dt_iseB3, overwrite = T)
 
