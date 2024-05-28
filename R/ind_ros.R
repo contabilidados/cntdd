9f5911c9cd0ba39cb324eb1fa269a28c320095d3
kleberformiga <mirandakf@ufersa.edu.br>
2024-05-28 11:10:15 -0300

    Ajuste dos arquivos de descrição

--- R/ind_ros.R
+++ R/ind_ros.R
@@ -1,25 +1,28 @@
-#' Análise Dupont (ROE desmembrado)
+#' Índice de Retorno sobre as Vendas (ROS)
 #'
 #' @description
-#' Essa função calcula o Índice de Retorno sobre o Patrimônio Líquido, baseado
-#' em vetores relativos as contas de Lucro Liquido, receita, Ativo Total e
-#' endividamento.
+#'
+#' Essa função calcula o Índice de Retorno sobre as Vendas baseado em vetores relativos às
+#' contas de Lucro e Receitas. O usuário definirá se usará lucro líquido ou
+#' outra medida de lucro, conforme literatura de interesse. Da mesma forma,
+#' escolherá entre receita líquida ou bruta. A literatura, especialmente sobre
+#' análise Dupont, trata essa medida como **margem**.
 #'
 #' @details
 #' Apresenta como resultado uma lista com 5 itens:
 #'
 #' 1. **Gráfico** se o parâmetro `plot` for `TRUE` ou `T`, mostra um gráfico com a
-#' evolução do ROE da empresa durante os períodos. Se for `FALSE` ou `F`,
+#' evolução do ROS da empresa durante os períodos. Se for `FALSE` ou `F`,
 #' o gráfico não é apresentado;
 #'
 #' 2.  **Contas** que corresponde ao banco de dados com as contas informadas para
 #' cálculo do indicador;
 #'
-#' 3.  **Índice** o índice de Retorno sobre o patrimônio líquido dos períodos informados;
+#' 3.  **Índice** o ROS dos períodos informados;
 #'
 #' 4.  **Análise Vertical** Análise Vertical das contas informadas no item 2.
-#' Contas de resultado terão suas análises verticais em relação à receita total e
-#' contas patrimoniais terão suas análises verticais em relação ao ativo total;
+#' Contas de resultado terão suas analises verticais em relação à receita total e
+#' contas patrimoniais terão suas analises verticais em relação ao ativo total;
 #'
 #' 5.  **Análise Horizontal** Análise Horizontal das contas informadas no item 2.
 #'
@@ -28,7 +31,7 @@
 #'
 #' Informações adicionais sobre como usar o pacote, orientamos acessar o menu
 #' `cntdd` do Blog do Projeto contabiliDados: <https://contabilidados.quarto.pub/>.
-#' Ao acessar, fazer busca pelo nome da função `ind_dupont`
+#' Ao acessar, fazer busca pelo nome da função `ind_ros`
 #'
 #' Contatos pelo email do Projeto contabiliDados:
 #' Email: <contabilidados@@ufersa.edu.br>
@@ -36,23 +39,19 @@
 #'
 #' @param indicador Um vetor tipo character com o nome do indicador
 #' @param periodo Vetor numérico indicando o período da análise
-#' @param lucLiq Vetor com os valores do lucro líquido da empresa
-#' @param patLiq vetor com os valores do patrimônio líquido da empresa
-#' @param ativoTotal Vetor com os valores do ativo total da empresa
-#' @param receitaTotal Vetor com os valores da receita total da empresa
+#' @param lucro Vetor com os valores do lucro da empresa
+#' @param receitaTotal Vetor com os valores da receita líquida da empresa
 #' @param plot Mostra gráfico? (TRUE/FALSE)
-#' @param relatorio Se `TRUE`, Mostra relatório do indicador. Se `FALSE`, mostra
-#' apenas o vetor com resultados do indicador (TRUE/FALSE)
+#' @param relatorio Se `TRUE`, Mostra relatório do indicador. Se `FALSE`,
+#' mostra apenas o vetor com resultados do indicador (TRUE/FALSE)
 #'
 #' @examples
 #' library(cntdd)
 #'
-#' ind_dupont(
-#'  indicador = "ROE_Dupont",
+#' ind_ros(
+#'  indicador = "ROS",
 #'  periodo = 2021:2022,
-#'  lucLiq = c(80,100),
-#'  patLiq = c(180,190),
-#'  ativoTotal = c(200,300),
+#'  lucro = c(8,10),
 #'  receitaTotal = c(300,500),
 #'  plot = TRUE,
 #'  relatorio = TRUE
@@ -65,74 +64,47 @@
 #' @importFrom stats na.omit
 #' @export
 
-ind_dupont <-
+ind_ros <-
   function(
-    indicador = "ROE_Dupont",
+    indicador = "ROS",
     periodo = (year(Sys.Date())-2):(year(Sys.Date())-1),
-    lucLiq = c(80,100),
-    patLiq = c(180,190),
-    ativoTotal = c(200,300),
+    lucro = c(8,10),
     receitaTotal = c(300,500),
     plot = T,
     relatorio = T
     ){
 
-  margemLuc <-
-    lucLiq / receitaTotal
-
-  giroAtivo <-
-    receitaTotal / ativoTotal
-
-  alavancagemFin <-
-    ativoTotal / patLiq
-
-  ROA <-
-    margemLuc * giroAtivo
-
   ratio <-
-    ROA * alavancagemFin
+    lucro / receitaTotal
 
   dt <- data.frame(Periodo = periodo, ratio = ratio)
   names(dt)[2] <- indicador
 
-
+  grafico <- ind_plots(dt, indicador)
 
   if(plot & relatorio){
-    print(ind_plots(dt, indicador))
+    print(grafico)
   }
 
   dtGeral <-
     data.frame(
       periodo     = periodo,
-      receitaTotal = receitaTotal,
-      lucLiq      = lucLiq,
-      patLiq      = patLiq,
-      ativoTotal    = ativoTotal,
-      margemLuc   = margemLuc,
-      giroAtivo   = giroAtivo,
-      ROA        = ROA
+      lucro      = lucro,
+      receitaTotal = receitaTotal
     ) %>%
     mutate(
       ratio = ratio
     ) %>%
     arrange(periodo) %>%
     mutate(
-      AH.receitaTotal   = round(receitaTotal / lag(receitaTotal) - 1, 4),
-      AH.lucLiq   = round(lucLiq / lag(lucLiq) - 1, 4),
-      AH.patLiq    = round(patLiq / lag(patLiq) - 1, 4),
-      AH.ativoTotal    = round(ativoTotal / lag(ativoTotal) - 1, 4),
-      AH.margemLuc    = round(margemLuc / lag(margemLuc) - 1, 4),
-      AH.giroAtivo    = round(giroAtivo / lag(giroAtivo) - 1, 4),
-      AH.ROA    = round(ROA / lag(ROA) - 1, 4),
-      AV.receitaTotal    = receitaTotal / receitaTotal,
-      AV.lucLiq    = lucLiq / receitaTotal,
-      AV.patLiq   = patLiq/ ativoTotal
+      AH.lucro   = round(lucro / lag(lucro) - 1, 4),
+      AH.receitaTotal    = round(receitaTotal / lag(receitaTotal) - 1, 4),
+      AV.lucro    = lucro / receitaTotal
     ) %>%
     rename_with(~ indicador, all_of("ratio"))
 
-
     dtGeral %>%
-      select(periodo:ROA) %>%
+      select(periodo:receitaTotal) %>%
       pivot_longer(cols = -periodo, names_to = "conta", values_to = "value") %>%
       na.omit() %>%
     pivot_wider(names_from = "periodo", values_from = "value") -> contas
@@ -157,12 +129,12 @@ ind_dupont <-
     na.omit() %>%
     pivot_wider(names_from = "periodo", values_from = "value") -> bdAH
 
-
   listaRelatorio <-
     list(
       Contas = contas, `Indice` = ratio,
       `Analise Vertical` = bdAV,
-      `Analise Horizontal` = bdAH)
+      `Analise Horizontal` = bdAH,
+      plot = grafico)
 
   apenasVetor <-
     ratio %>%
@@ -175,6 +147,4 @@ ind_dupont <-
   } else {
     return(apenasVetor)
   }
-
 }
-
